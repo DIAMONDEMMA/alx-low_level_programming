20a81faa6c8273fe971085715eb959904290c2fc
diamondemma <okaforemmanuel90@yahoo.com>
2022-09-09 05:13:24 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,10 @@
 #include <stdio.h>
-int main(void) { write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+
+/**
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n",61);
+	return (1);
