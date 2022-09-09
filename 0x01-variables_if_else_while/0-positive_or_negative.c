77b975f586fc36b16a1e43585a03c8dcfe0773e8
diamondemma <okaforemmanuel90@yahoo.com>
2022-09-09 07:45:27 -0700

    c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#!/bin/bash
+ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-positive_or_negative.c -o 0-positive_or_negative
+#include<stdio.h>
+
+ /**
+  * main - prints the phrase with proper grammar,
+  * but the outcome is a piece of art,
+  * Return: 0 if exited properly, non-zero otherwise
+  */
+ int main(void)
+{
+	./0-positive_or_negative,
+	./0-positive_or_negative,
+	./0-positive_or_negative,
+	./0-positive_or_negative,
+	./0-positive_or_negative,
+	./0-positive_or_negative,
+	./0-positive_or_negative,
+	return (0);
+}
