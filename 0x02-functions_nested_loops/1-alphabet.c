dc47a952f8c9d77c4b37f95dd290f526a9a5723f
diamondemma <okaforemmanuel90@yahoo.com>
2022-09-13 20:55:18 -0700

    c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * Description:print lowercase alphabets
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
