2545f77abdd4e77372c061a533c79a9757ee3e57
diamondemma <okaforemmanuel90@yahoo.com>
2022-09-15 10:18:59 -0700

    c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,14 +11,20 @@
 
 int largest_number(int a, int b, int c)
 {
-int largest;
-if (a > b && b > c)
-{
-largest = a;
-}
-else
-{
-largest = c;
-}
-return (largest);
+	int largest;
+
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+
+	return (largest);
 }
