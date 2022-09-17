c169904747d08617b97c9adcb24de9e9e572e016
diamondemma <okaforemmanuel90@yahoo.com>
2022-09-17 03:34:54 -0700

    c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,10 +2,12 @@
 
 #define MAIN_H
 
+
 #include <stdio.h>
 
 void positive_or_negative(int i);
 
-largest = largest_number(a, b, c);
+
+int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
