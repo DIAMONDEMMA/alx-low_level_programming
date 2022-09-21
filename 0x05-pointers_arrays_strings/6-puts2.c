5953a75054be10f46b94000ee308343bd55259b0
diamondemma <okaforemmanuel90@yahoo.com>
2022-09-21 11:03:01 -0700

    c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,22 +1,34 @@
-#include <stdio.h>
 #include "main.h"
+/**
+ *  * _strlen - returns the length of string
+ *   * @s: string s
+ *    * Return: length of string
+ *     */
+int _strlen(char *s)
+{
+
+		int len = 0;
+
+			while (*s)
+					{
+								s++;
+										len++;
+											}
+				return (len);
+}
 
 /**
- * puts2 - prints every other character
- * @str: string
- *
- * Return: nothing
- */
-
+ *  * puts2 - prints one char out of 2 of a string
+ *   *
+ *    * @str: string str
+ *     */
 void puts2(char *str)
 {
-int i = 0;
+		int x;
+			int size = _strlen(str);
 
-while (*(str + 1) != '\0')
-{
-if (i % 2 == 0)
-putchar(*(str + 1));
-i++;
-}
-putchar(10);
+				for (x = 0; x < size; x += 2)
+							_putchar(*(str + x));
+
+					_putchar('\n');
 }
