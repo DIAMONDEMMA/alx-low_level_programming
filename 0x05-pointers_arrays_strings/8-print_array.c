c79f56e88b32fc546ac3958af3503610eaa21b80
diamondemma <okaforemmanuel90@yahoo.com>
2022-09-21 11:14:24 -0700

    c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,24 +1,24 @@
-#include <stdio.h>
 #include "main.h"
-
+#include <stdio.h>
 /**
- * print_array - prints n element of array
- * @a: array
- * @n: number of elements
- * Return: nothing
- */
-
+ *  * print_array - prints `n` elements of an array of integers
+ *   * @a: pointer to an int type array
+ *    * @n: Array to be printed
+ *     *
+ *      * Return: Always 0
+ *       */
 void print_array(int *a, int n)
 {
-int inc;
+		int i = 0;
 
-if (inc = 0; inc < n ; inc++)
-{
-if (inc != n - 1)
-printf("%d, ", a[inc]);
-else
-printf("%d", a[inc]);
-}
-putchar(10);
+			for (n--; n >= 0; n--, i++)
+					{
+								printf("%d", a[i]);
+										if (n > 0)
+													{
+																	printf(", ");
+																			}
+											}
+				printf("\n");
 }
 
