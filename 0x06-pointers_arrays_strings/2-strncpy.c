621f8a16213cfcb85ce7618a629b95f79b5eb331
diamondemma <okaforemmanuel90@yahoo.com>
2022-09-22 17:55:45 -0700

    c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,13 +2,13 @@
 #include <string.h>
 
 /**
- *  * _strncpy -> function to copy string
- *   * @dest: first parameter
- *    * @src: second parameter
- *     * @n: third parameter
- *      *
- *       * Return: string
- *        */
+ * _strncpy -> function to copy string
+ * @dest: first parameter
+ * @src: second parameter
+ * @n: third parameter
+ *
+ * Return: string
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 		strncpy(dest, src, n);
