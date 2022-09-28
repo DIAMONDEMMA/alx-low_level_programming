b3e651d7c746fe130bce9768f67a1f743f2cb9b2
diamondemma <okaforemmanuel90@yahoo.com>
2022-09-27 20:57:56 -0700

    c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,27 +10,19 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j, f, flag;
+	unsigned int i, j;
 
-	f = 0;
-
-	for (i = 0; s[i] != '\0' i++)
+	for (i = 0; s[i]; i++)
 	{
-		flag = 0;
-		for (j = 0; accept[j] != '\0' j++)
+		for (j = 0; accept[j]; j++)
 		{
 			if (s[i] == accept[j])
-			{
-				f++;
-				flag = 1;
-			}
+				break;
 		}
-		if (flag == 0)
-		{
-			return (F);
-		}
+		if (!accept[j])
+			break;
 	}
-	return (0);
+	return (i);
 }
 
 
