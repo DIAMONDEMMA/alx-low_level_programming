c11fac848c76676a2d3804507fa4527a997c8dfb
diamondemma <okaforemmanuel90@yahoo.com>
2022-09-27 21:50:06 -0700

    c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,24 +10,17 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	unsigned int i =0, j = 0;
+	int i, j;
 
-	while (haystack[i])
+	for (i = 0; haystack[i] != '\0' i++)
 	{
-		while (needle[j] && (heystack[i] == neddle[0]))
+		for (j = 0; neddle[j] != '\0' j++)
 		{
-			if (haystack(i + j) == needle[j])
-				j++;
-			else
+			if (haystack[i + j] != needle[j])
 				break;
 		}
-		if (needle[j])
-		{
-			i++;
-			j = 0;
-		}
-		else
-			return (haystack + 1);
+		if (!needle[j])
+			return (&haystack[i]);
 	}
-	return (0);
+	return (NULL);
 }
